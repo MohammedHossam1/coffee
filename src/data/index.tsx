660066f73ba8f0891fa0e49d54baa133ca3687b1
mohammedhossam1 <mohammedhossam199998@gmail.com>
2025-07-22 20:59:02 +0200

    fixed imgs

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,17 +1,21 @@
 import type { IProduct } from "../interfaces";
 
+// استيراد الصور
+import img1 from "../assets/1.png";
+import img2 from "../assets/2.png";
+import img3 from "../assets/3.png";
+import img4 from "../assets/4.png";
+import img7 from "../assets/7.png";
+import img11 from "../assets/11.png";
+import img22 from "../assets/22.png";
+import img33 from "../assets/33.png";
 
+// بيانات المنتجات
 export const productsData: IProduct[] = [
   {
     id: 1,
     title: "Espresso",
-    images: [
-    "/src/assets/1.png",
-    "/src/assets/2.png",
-    "/src/assets/3.png",
-    "/src/assets/4.png",
-    "/src/assets/7.png",
-    ],
+    images: [img1, img2, img3, img4, img7],
     price: 45,
     category: "Espresso",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
@@ -19,13 +23,7 @@ export const productsData: IProduct[] = [
   {
     id: 2,
     title: "Cappuccino",
-    images: [
-      "/src/assets/7.png",
-      "/src/assets/1.png",
-      "/src/assets/2.png",
-      "/src/assets/3.png",
-      "/src/assets/4.png",
-    ],
+    images: [img7, img1, img2, img3, img4],
     price: 55,
     category: "Milk Coffee",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
@@ -33,13 +31,7 @@ export const productsData: IProduct[] = [
   {
     id: 3,
     title: "Latte",
-    images: [
-      "/src/assets/4.png",
-      "/src/assets/1.png",
-      "/src/assets/2.png",
-      "/src/assets/3.png",
-      "/src/assets/7.png",
-    ],
+    images: [img4, img1, img2, img3, img7],
     price: 50,
     category: "Milk Coffee",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
@@ -47,13 +39,7 @@ export const productsData: IProduct[] = [
   {
     id: 4,
     title: "Mocha",
-    images: [
-      "/src/assets/3.png",
-      "/src/assets/1.png",
-      "/src/assets/2.png",
-      "/src/assets/4.png",
-      "/src/assets/7.png",
-    ],
+    images: [img3, img1, img2, img4, img7],
     price: 60,
     category: "Chocolate",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
@@ -61,13 +47,7 @@ export const productsData: IProduct[] = [
   {
     id: 5,
     title: "Americano",
-    images: [
-      "/src/assets/1.png",
-      "/src/assets/2.png",
-      "/src/assets/3.png",
-      "/src/assets/4.png",
-      "/src/assets/7.png",
-    ],
+    images: [img1, img2, img3, img4, img7],
     price: 40,
     category: "Black Coffee",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
@@ -75,13 +55,7 @@ export const productsData: IProduct[] = [
   {
     id: 6,
     title: "Turkish Coffee",
-    images: [
-      "/src/assets/1.png",
-      "/src/assets/2.png",
-      "/src/assets/3.png",
-      "/src/assets/4.png",
-      "/src/assets/7.png",
-    ],
+    images: [img1, img2, img3, img4, img7],
     price: 35,
     category: "Turkish",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
@@ -89,108 +63,65 @@ export const productsData: IProduct[] = [
   {
     id: 7,
     title: "Iced Coffee",
-    images: [
-      "/src/assets/1.png",
-      "/src/assets/2.png",
-      "/src/assets/3.png",
-      "/src/assets/4.png",
-      "/src/assets/7.png",
-    ],
+    images: [img1, img2, img3, img4, img7],
     price: 48,
     category: "Cold",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
-    },
+  },
 ];
 
 export const productsData2: IProduct[] = [
   {
-    id: 6,
+    id: 8,
     title: "Turkish Coffee",
-    images: [
-      "/src/assets/11.png",
-      "/src/assets/2.png",
-      "/src/assets/3.png",
-      "/src/assets/4.png",
-      "/src/assets/7.png",
-    ],
+    images: [img11, img2, img3, img4, img7],
     price: 35,
     category: "Turkish",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
   },
   {
-    id: 6,
+    id: 9,
     title: "Turkish Coffee",
-    images: [
-      "/src/assets/22.png",
-      "/src/assets/2.png",
-      "/src/assets/3.png",
-      "/src/assets/4.png",
-      "/src/assets/7.png",
-    ],
+    images: [img22, img2, img3, img4, img7],
     price: 35,
     category: "Turkish",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
   },
   {
-    id: 6,
+    id: 10,
     title: "Turkish Coffee",
-    images: [
-      "/src/assets/33.png",
-      "/src/assets/2.png",
-      "/src/assets/3.png",
-      "/src/assets/4.png",
-      "/src/assets/7.png",
-    ],
+    images: [img33, img2, img3, img4, img7],
     price: 35,
     category: "Turkish",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
   },
   {
-    id: 6,
+    id: 11,
     title: "Turkish Coffee",
-    images: [
-      "/src/assets/11.png",
-      "/src/assets/2.png",
-      "/src/assets/3.png",
-      "/src/assets/4.png",
-      "/src/assets/7.png",
-    ],
+    images: [img11, img2, img3, img4, img7],
     price: 35,
     category: "Turkish",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
   },
   {
-    id: 6,
+    id: 12,
     title: "Turkish Coffee",
-    images: [
-      "/src/assets/22.png",
-      "/src/assets/2.png",
-      "/src/assets/3.png",
-      "/src/assets/4.png",
-      "/src/assets/7.png",
-    ],
+    images: [img22, img2, img3, img4, img7],
     price: 35,
     category: "Turkish",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
   },
   {
-    id: 6,
+    id: 13,
     title: "Turkish Coffee",
-    images: [
-      "/src/assets/33.png",
-      "/src/assets/2.png",
-      "/src/assets/3.png",
-      "/src/assets/4.png",
-      "/src/assets/7.png",
-    ],
+    images: [img33, img2, img3, img4, img7],
     price: 35,
     category: "Turkish",
     desc: "A rich and bold espresso, perfectly concentrated and served in a small cup for an intense coffee experience.",
   },
-  
- 
 ];
 
+// كاتيجوري متعددة
 export const categoriesData = [
   { id: 1, name: "برجر" },
   { id: 2, name: "بيتزا" },
@@ -205,9 +136,10 @@ export const categoriesData = [
   { id: 11, name: "حلويات" },
   { id: 12, name: "فراخ" },
 ];
-// data/categories.ts
+
+// كاتيجوري بالك صور
 export const categories = [
-  { id: 1, name: "قهوة", image: "/src/assets/2.png" },
-  { id: 2, name: "شاي", image: "/src/assets/3.png" },
-  { id: 3, name: "عصير", image: "/src/assets/4.png" },
+  { id: 1, name: "قهوة", image: img2 },
+  { id: 2, name: "شاي", image: img3 },
+  { id: 3, name: "عصير", image: img4 },
 ];
