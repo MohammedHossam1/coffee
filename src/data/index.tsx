aeee78287cfc4d25dc8fcb2c48e3d5bdd4f5de51
mohammedhossam1 <mohammedhossam199998@gmail.com>
2025-08-03 18:17:58 +0200

    Fixed build

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,4 +1,4 @@
-import type { ICategory, IProduct } from "../interfaces";
+import type { ICategory } from "../interfaces";
 
 // استيراد الصور
 import img111 from "/src/assets/slide-main.png"
@@ -10,83 +10,9 @@ import img1 from "../assets/1.png";
 import img2 from "../assets/2.png";
 import img3 from "../assets/3.png";
 import img4 from "../assets/4.png";
-import img7 from "../assets/7.png";
 
-// بيانات المنتجات
-export const productsData: IProduct[] = [
-  {
-    id: 1,
-    title: "إسبريسو",
-    images: [img1, img2, img3, img4, img7],
-    price: 45,
-    category: "إسبريسو",
-    desc: "اكتشف متعة القهوة كما لم تعشها من قبل. نكهات غنية، روائح تأسر الحواس، وتجربة تُسحر بها من أول فنجان... فهل أنت جاهز؟",
-  },
-  {
-    id: 2,
-    title: "كابتشينو",
-    images: [img7, img1, img2, img3, img4],
-    price: 55,
-    category: "قهوة بالحليب",
-    desc: "اكتشف متعة القهوة كما لم تعشها من قبل. نكهات غنية، روائح تأسر الحواس، وتجربة تُسحر بها من أول فنجان... فهل أنت جاهز؟",
-  },
-  {
-    id: 3,
-    title: "لاتيه",
-    images: [img4, img1, img2, img3, img7],
-    price: 50,
-    category: "قهوة بالحليب",
-    desc: "اكتشف متعة القهوة كما لم تعشها من قبل. نكهات غنية، روائح تأسر الحواس، وتجربة تُسحر بها من أول فنجان... فهل أنت جاهز؟",
-  },
-  {
-    id: 4,
-    title: "موكا",
-    images: [img3, img1, img2, img4, img7],
-    price: 16.99,
-    category: "شوكولاتة",
-    desc: "اكتشف متعة القهوة كما لم تعشها من قبل. نكهات غنية، روائح تأسر الحواس، وتجربة تُسحر بها من أول فنجان... فهل أنت جاهز؟",
-  },
-  {
-    id: 5,
-    title: "أمريكانو",
-    images: [img1, img2, img3, img4, img7],
-    price: 16.99,
-    category: "قهوة سوداء",
-    desc: "اكتشف متعة القهوة كما لم تعشها من قبل. نكهات غنية، روائح تأسر الحواس، وتجربة تُسحر بها من أول فنجان... فهل أنت جاهز؟",
-  },
-  {
-    id: 6,
-    title: "قهوة تركية",
-    images: [img1, img2, img3, img4, img7],
-    price: 16.99,
-    category: "تركي",
-    desc: "اكتشف متعة القهوة كما لم تعشها من قبل. نكهات غنية، روائح تأسر الحواس، وتجربة تُسحر بها من أول فنجان... فهل أنت جاهز؟",
-  },
-  {
-    id: 7,
-    title: "قهوة مثلجة",
-    images: [img1, img2, img3, img4, img7],
-    price: 16.99,
-    category: "باردة",
-    desc: "اكتشف متعة القهوة كما لم تعشها من قبل. نكهات غنية، روائح تأسر الحواس، وتجربة تُسحر بها من أول فنجان... فهل أنت جاهز؟",
-  },
-];
 
-// كاتيجوري متعددة
-export const categoriesData = [
-  { id: 1, name: "برجر" },
-  { id: 2, name: "بيتزا" },
-  { id: 3, name: "سوشي" },
-  { id: 4, name: "حلويات" },
-  { id: 5, name: "فراخ" },
-  { id: 6, name: "باستا" },
-  { id: 7, name: "آسيوي" },
-  { id: 8, name: "برجر" },
-  { id: 9, name: "بيتزا" },
-  { id: 10, name: "سوشي" },
-  { id: 11, name: "حلويات" },
-  { id: 12, name: "فراخ" },
-];
+
 
 // كاتيجوري بالك صور
 export const categories: ICategory[] = [
